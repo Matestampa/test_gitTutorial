c50bf06494e0b16a91a44d4dd64e0c77033cf992
Matestampa <paredpiso5@gmail.com>
2024-06-27 20:01:50 -0300

    Commit 1 feature 1

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-console.log("Segundo Commit")
-
-let tute="";
 
 function feature_1(){
     console.log("La feature-1 para la branch feature-1")
-}
-
-function feature_2(){
-    console.log("La feature-2 para la branch feature-2")
-}
-
-function feature_3(){
-    console.log("La feature-3 para la branch feature-3")
 }
