1a0c033d699d0ee27b995f27fac1be07b822f8d9
Matestampa <paredpiso5@gmail.com>
2026-01-31 22:06:43 -0300

    test withput npm update

--- src/db/mongodb/models.js
+++ src/db/mongodb/models.js
@@ -5,4 +5,7 @@ const {goalSchema}=require("@matestampa/diffum-goals_mongoose-schemas");
  // Creación del modelo a partir del esquema
 const GoalModel = mongoose.model('Goal', goalSchema);
 
+
+console.log(goalSchema.tree)
+
 module.exports= {GoalModel};
